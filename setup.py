456dd385dd5797adfde1ff2c834a58ce4b7c076c
HenrryVargas <vs.henrry@gmail.com>
2024-04-08 10:21:53 +0200

    plantilla data science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='iastronglen',
+    packages=find_packages(),
+    version='0.0.1',
+    description='A short description of this project.',
+    author='Your name (or your organization/company/team)',
+    license='MIT',   
+)
